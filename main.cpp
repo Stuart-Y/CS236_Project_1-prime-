251c93518f03188fca500912ecbec46139937558
Stuart-Y <78181608+Stuart-Y@users.noreply.github.com>
2021-01-29 19:04:54 -0700

    testing file grabbing commented out the complex bits for now

--- main.cpp
+++ main.cpp
@@ -5,10 +5,15 @@
 #include "Lexer.h"
 #include "Token.h"
 using namespace std;
-int main(int argc, char* argv[]) {
-	string fileName = argv[1];
+int main(/*int argc, char* argv[]*/) {
+	/*string fileName = argv[1];
 	ifstream input(fileName);
 	cout<< input.rdbuf();
-	//Lexer process = new Lexer();
-	//process.Run(input);
+	Lexer process = new Lexer();
+	process.Run(input);*/
+	ifstream input("in10.txt");
+	string out;
+	input >> out;
+	cout << out;
+	return 0;
 }
